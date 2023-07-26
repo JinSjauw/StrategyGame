048b04eea5a598859c8f1280f746d9bbcfa440cd
JinSjauw <jinzhouliu4@gmail.com>
2023-07-26 18:56:52 +0200

    Basic recoil and spread

--- Assets/CrosshairController.cs
+++ Assets/CrosshairController.cs
@@ -12,21 +12,29 @@ public class CrosshairController : MonoBehaviour
     [SerializeField] private Transform southReticle;
     [SerializeField] private Transform westReticle;
     [SerializeField] private Transform eastReticle;
+
+    [SerializeField] private float reticleOffset;
     
     //Variables
     [SerializeField] private Unit unit;
 
+    //From weapon. When you fire in the weapon the bullet wil get an random angle as forward direction.
+    //Only representation in the class.
     [SerializeField] private float _accuracy;
+    
     [SerializeField] private float _currentSpread;
     [SerializeField] private float _maxSpread;
     [SerializeField] private float _minSpread;
     [SerializeField] private float returnSpeed;
     [SerializeField] private float reticleReturnSpeed;
-    
+
     [SerializeField] private AnimationCurve returnCurve;
     [SerializeField] private AnimationCurve reticleReturnCurve;
     
     [SerializeField] private Vector2 _center;
+
+    [SerializeField] private Transform red;
+    [SerializeField] private Transform green;
     
     private Vector2 _mousePosition;
     
@@ -56,18 +64,29 @@ public class CrosshairController : MonoBehaviour
         Quaternion rotationA = Quaternion.AngleAxis(_accuracy / 2, Vector3.forward);
         Quaternion rotationB = Quaternion.AngleAxis(-(_accuracy / 2), Vector3.forward);
 
-        Vector3 direction = transform.position - unit.transform.position;
-        Debug.Log(direction.normalized);
+        Vector2 direction =  transform.position- unit.transform.position;
+        Debug.Log(direction.normalized + " " + _mousePosition);
         
-        Debug.DrawLine(unit.transform.position, _mousePosition, Color.blue);
-        Debug.DrawLine(unit.transform.position, direction.normalized * distance, Color.green);
+        Debug.DrawLine(unit.transform.position, transform.position, Color.blue);
+        //Debug.DrawRay(unit.transform.position, direction.normalized, Color.green);
 
-        /*Vector3 rotatedLineA = rotationA * direction * distance;
-        Vector3 rotatedLineB = rotationB * direction * distance;
+        Vector3 rotatedLineA = unit.transform.position + rotationA * direction.normalized * distance;
+        Vector3 rotatedLineB = unit.transform.position + rotationB * direction.normalized * distance;
         
         Debug.DrawLine(unit.transform.position, rotatedLineA, Color.red);
-        Debug.DrawLine(unit.transform.position, rotatedLineB, Color.green);*/
+        Debug.DrawLine(unit.transform.position, rotatedLineB, Color.green);
+        
+        
+        float deviationDistance = Vector2.Distance(rotatedLineA, rotatedLineB);
+        Debug.DrawRay(rotatedLineA, (rotatedLineB - rotatedLineA) * deviationDistance, Color.magenta);
 
+        _minSpread = deviationDistance / 2;
+        
+        red.up = direction.normalized;
+        green.up = direction.normalized;
+
+        red.transform.position = rotatedLineA;
+        green.transform.position = rotatedLineB;
         //Draw 2 angled line vector (angle = accuracy / 2)
         //Use distance between the 2 angled vectors as a radius for reticle and spread.
         //This way it increases the further you go.
@@ -75,8 +94,8 @@ public class CrosshairController : MonoBehaviour
 
         _reticleReturnCurrent = Mathf.MoveTowards(_reticleReturnCurrent, 1, reticleReturnSpeed * Time.deltaTime);
 
-        _currentSpread = Mathf.Lerp(_maxSpread, _minSpread, reticleReturnCurve.Evaluate(_reticleReturnCurrent));
-        
+        _currentSpread = Mathf.Lerp(_maxSpread, _minSpread, reticleReturnCurve.Evaluate(_reticleReturnCurrent)) + reticleOffset;
+
         northReticle.localPosition = new Vector3(0, _currentSpread, 0);
         southReticle.localPosition = new Vector3(0, -_currentSpread, 0);
         
